abf5b95a1a69426a26ad5d25c21c19aeadf17605
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-05-18 12:22:03 +0900

    Pick a date

--- app.js
+++ app.js
@@ -30,19 +30,39 @@ class Channels extends React.Component {
     }
     handleChannelSelect(e) {
         e.preventDefault();
-        console.log("Channel selected: " + $(e.target).data("name"));
+        let channel = this.state.data[$(e.target).data("index")];
+        ReactDOM.render(React.createElement(DateSelector, channel), document.getElementById('date'));
     }
     render() {
         if(! this.state) return <div>?? no state</div>;
         if(this.state.message) {
             return (<div>{this.state.message}</div>);
         } else {
-            let nodes = this.state.data.map(c => {
-              return (<li key={c.id}><a onClick={this.handleChannelSelect} href="#" data-name={c.name}>#{c.name}</a></li>);
+            let nodes = this.state.data.map((c,i) => {
+              return (<li key={c.id}><a onClick={this.handleChannelSelect.bind(this)} href="#" data-index={i}>#{c.name}</a></li>);
             })
             return (<ul>{nodes}</ul>);
         }
     }
 };
+
+class DateSelector extends React.Component {
+    constructor() {
+        super();
+    }
+    handleDateChange(e) {
+        console.log(e)
+    }
+    render() {
+        let created = new Date(this.props.created*1000);
+        let now = new Date();
+        let pad2 = n => { return ("0"+n).slice(-2); };
+        let toDateString = date => { return date.getFullYear() + "-" + pad2(date.getMonth()+1) + "-" + pad2(date.getDate()); };
+        return (<div>
+            <p>Pick a date for channel #{this.props.name}</p>
+            <input type="date" onChange={this.handleDateChange.bind(this)} min={toDateString(created)} max={toDateString(now)}/>
+        </div>);
+    }
+}
 
 ReactDOM.render(<Channels />, document.getElementById('channels'));
