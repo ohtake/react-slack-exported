b55ed1c605b5d7065b0f3ca390da43452aeeb28a
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-06-03 12:03:34 +0900

    Apply FlatButton to channel selector

--- src/channel_selector.jsx
+++ src/channel_selector.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
+import FlatButton from 'material-ui/FlatButton';
 import * as util from './util.js';
 
 const ChannelSelector = (props) => {
   let nodes = props.route.channelResolver.listChannels().map(c =>
-    <li key={c.id}><Link to={`/channel/${c.name}`} activeClassName="active">#{c.name}</Link></li>
+    <Link to={`/channel/${c.name}`}>
+      <FlatButton key={c.id} primary={c.name === props.params.channelName}>#{c.name}</FlatButton>
+    </Link>
   );
-  return <div><h2>Channels</h2><ul id="channels">{nodes}</ul>{props.children}</div>;
+  return <div><h2>Channels</h2><div>{nodes}</div>{props.children}</div>;
 };
 ChannelSelector.propTypes = util.propTypesRoute;
 
