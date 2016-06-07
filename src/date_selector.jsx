277c347294dd7b0a2bb0d6c5a49f0dff52473000
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-06-07 11:52:10 +0900

    Use min/max date from channel summary

--- src/date_selector.jsx
+++ src/date_selector.jsx
@@ -7,17 +7,28 @@ export default class DateSelector extends React.Component {
   constructor(props) {
     super();
 
-    this.state = this.propsToState(props);
+    this.propsToState(props);
 
     this.handleDateChange = this.handleDateChange.bind(this);
   }
   componentWillReceiveProps(nextProps) {
-    this.setState(this.propsToState(nextProps));
+    this.propsToState(nextProps);
   }
   propsToState(props) {
     const channel = props.route.channelResolver.find(props.params.channelName);
     const date = this.dateStringToDate(props.params.date);
-    return { channel, date };
+    window.fetch(`assets/channel_summary/${channel.name}.json`)
+    .then(util.checkStatus)
+    .then(util.parseJSON)
+    .then(data => {
+      const minDate = data.counts.reduce((prevValue, currentValue) =>
+        ((prevValue && prevValue < currentValue.date) ? prevValue : currentValue.date)
+      , null);
+      const maxDate = data.counts.reduce((prevValue, currentValue) =>
+        ((prevValue && prevValue > currentValue.date) ? prevValue : currentValue.date)
+      , null);
+      this.setState({ channel, minDate: this.dateStringToDate(minDate), maxDate: this.dateStringToDate(maxDate), date });
+    });
   }
   dateStringToDate(str) {
     if (! str) return null;
@@ -28,19 +39,16 @@ export default class DateSelector extends React.Component {
     this.context.router.push(`/channel/${this.state.channel.name}/date/${moment(date).format('YYYY-MM-DD')}`);
   }
   render() {
-    const created = new Date(this.state.channel.created * 1000);
-    const now = new Date();
-    const toSlackDate = date => {
-      const m = moment(date).tz('America/Los_Angeles');
-      return new Date(m.year(), m.month(), m.date());
-    };
+    if (! this.state || ! this.state.channel) {
+      return <div>Loading {this.props.params.channelName} channel info.</div>;
+    }
     return (<div>
-      <p>{this.state.channel.name} channel</p>
+      <p>{this.state.channel.name} channel ({moment(this.state.minDate).format('YYYY-MM-DD')} - {moment(this.state.maxDate).format('YYYY-MM-DD')})</p>
       <DatePicker
         ref="input"
         floatingLabelText="PST/PDT date"
-        minDate={toSlackDate(created)}
-        maxDate={toSlackDate(now)}
+        minDate={this.state.minDate}
+        maxDate={this.state.maxDate}
         onChange={this.handleDateChange}
         value={this.state.date}
       />
